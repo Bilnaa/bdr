d9573bfba3483d0217add2820eae9dedd6c5026e
Bilnaa <noufele.mehdi@hotmail.fr>
2023-05-09 01:49:05 +0200

    feat: added the specific routes

--- index.php
+++ index.php
@@ -1,21 +1,70 @@
 <?php
 
-session_start();
-
 require_once 'router.php';
 
+session_start();
 $router = new Router();
 
-$router->add('/bdr/home', function () {
-    echo "Welcome to the home page!";
+$router->add('/', function () {
+    if (isset($_SESSION['matricule'])) {
+        header('Location:  /dashboard');
+    } else {
+        header('Location:  /login');
+    }
 });
 
-$router->add('/bdr/login', function () {
-    include("login/index.html");
+$router->add('/login', function () {
+    if (isset($_SESSION['matricule'])) {
+        header('Location:  /dashboard');
+    }
+    include("login/index.php");
 });
 
-$router->add('/bdr/dashboard', function () {
+$router->add('/dashboard', function () {
+    if (!isset($_SESSION['matricule'])) {
+        header('Location:  /login');
+    }
     include('pages/dashboard.php');
+});
+
+$router->add('/logout', function () {
+    session_destroy();
+    header('Location:  /login');
+});
+
+$router->add('/profile', function () {
+    if (!isset($_SESSION['matricule'])) {
+        header('Location:  /login');
+    }
+    include('pages/profile.php');
+});
+
+$router->add('/demandes', function () {
+    if (!isset($_SESSION['matricule'])) {
+        header('Location:  /login');
+    }
+    include('pages/demandes.php');
+});
+
+$router->add('/annuaire', function () {
+    if (!isset($_SESSION['matricule'])) {
+        header('Location:  /login');
+    }
+    include('pages/annuaire.php');
+});
+
+$router->add('/formulaire', function () {
+    if (!isset($_SESSION['matricule'])) {
+        header('Location:  /login');
+    }
+    include('pages/demande_conge.php');
+});
+
+$router ->add('/validation', function () {
+    if (!isset($_SESSION['matricule'])) {
+        header('Location:  /login');
+    }
+    include('pages/validation.php');
 });
 
 $router->dispatch($_SERVER['REQUEST_URI']);
