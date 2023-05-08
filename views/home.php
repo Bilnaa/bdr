fd12a0313b3e21e88dd35a6136f5da826c22ec25
Bilnaa <noufele.mehdi@hotmail.fr>
2023-05-09 01:51:32 +0200

    del : no more home.php

--- views/home.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    if (isset($_SESSION['matricule'])) {
-        header('Location: http://localhost/bdr/dashboard');
-    } else {
-        header('Location: http://localhost/bdr/login');
-    }
-?>
