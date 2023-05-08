d42656098f82a5cbaf3e7b9a80d9c82a61998051
Bilnaa <noufele.mehdi@hotmail.fr>
2023-05-09 01:53:17 +0200

    changes : login

--- libs/connexion.php
+++ libs/connexion.php
@@ -1,66 +1,41 @@
 <?php
+session_start();
 require_once ("../models/database.php");
 require_once ("../utils/integrity.php");
-session_start();
 
+$URL = "/";
 if (isset($_POST['matricule']) && isset($_POST['password']) && !empty($_POST['matricule']) && !empty($_POST['password']) ){
+    $_SESSION['matricule'] = $_POST['matricule'];
     $username = $_POST['matricule'];
     $password = $_POST['password'];
-    $integrity = new Integrity();
-    $hashed_password = $integrity->hashPassword($password);
-    $pdo = new Database();
-    $pdo = $pdo->connect();
-
-    $sql = 'SELECT * FROM `employe` WHERE `matriculeemploye` = :matricule AND `passwordemploye` = :password';
-    $exec = $pdo->prepare($sql);
-    $exec->bindValue(':matricule', $username);
-    $exec->bindValue(':password', $password);
-    $exec->execute();
-    $result = $exec->fetch(PDO::FETCH_ASSOC);
-    if (!$result) {
-        $sql = 'SELECT * FROM `employe` WHERE `matriculeemploye` = :matricule';
-        $exec = $pdo->prepare($sql);
-        $exec->bindValue(':matricule', $username);
-        $exec->execute();
-        $result = $exec->fetch(PDO::FETCH_ASSOC);
-        if ($result) {
-            $hashed_password = $result['passwordemploye'];
-            $is_password_correct = $integrity->verify_password($password, $hashed_password);
-            if ($is_password_correct) {
-                // Update the password with the new hashed password
-                $integrity->store_hashed_password($username, $hashed_password);
-                // Check if it's the first connexion
-                if ($result['DDERNIERECOEMPLOYE'] == null) {
-                    echo "ok, first connexion";
-                    http_response_code(200);
-                    header('Location: http://localhost/bdr/login/premiereConnexion.php');
-                } else {
-                    echo "ok, not first connexion";
-                    http_response_code(200);
-                    header('Location: http://localhost/bdr/dashboard');
-                }
+    $db = new Database();
+    $connection = $db->connect();
+    $sql = "SELECT * FROM employe WHERE matriculeemploye = :matricule"; // check if the user exists
+    $stmt = $connection->prepare($sql);
+    $stmt->bindValue(':matricule', $username);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($result){
+        $user = $result;
+        $integrity = new Integrity();
+        $hashed_password = $result['PASSWORDEMPLOYE'];
+        $result = $integrity->verify_password($password, $hashed_password);
+        if($result){
+            $changed_password = $user['DDERNIERECOEMPLOYE'];
+            if($changed_password == null){
+                echo "You connected succesfully but you have to change your password !";
+                header('Location: '.$URL.'login/premiereConnexion.php');
             } else {
-                $error = 'Wrong username or password!';
-                echo $error;
-                http_response_code(403);
-                header('Location: http://localhost/bdr/login?error=1');
+                echo "You have already changed your password !";
+                header('Location: '.$URL.'dashboard');
             }
         } else {
-            $error = 'Wrong username or password!';
-            echo $error;
-            http_response_code(403);
-            header('Location: http://localhost/bdr/login?error=1');
+            echo "The password is incorrect !";
+            header('Location: '.$URL.'login?error=1');
         }
     } else {
-        if ($result['DDERNIERECOEMPLOYE'] == null) {
-            echo "ok, first connexion";
-            http_response_code(200);
-            header('Location: http://localhost/bdr/login/premiereConnexion.php');
-        } else {
-            echo "ok, not first connexion";
-            http_response_code(200);
-            header('Location: http://localhost/bdr/dashboard');
-        }
+        echo "The user does not exist !";
+        header('Location: '.$URL.'login?error=2');
     }
 }
 ?>
--- libs/firstConnexion.php
+++ libs/firstConnexion.php
@@ -1,22 +1,33 @@
 <?php
-
-require_once './utils/integrity.php';
-include("database.php");
+session_start();
+require_once('../utils/integrity.php');
+require_once('../models/database.php');
+$integrity = new Integrity();
 $pdo = new Database();
 $pdo = $pdo->connect();
 $sql = "UPDATE `employe` SET `DDERNIERECOEMPLOYE` = :date, `passwordemploye` = :password WHERE `matriculeemploye` = :matricule";
 $exec = $pdo->prepare($sql);
 $exec->bindValue(':matricule', $_SESSION['matricule']);
-$exec->bindValue(':password', $_POST['mdp']);
+$exec->bindValue(':password', $integrity->hashPassword($_POST['mdp']));
 $exec->bindValue(':date', date("Y-m-d"));
 $exec->execute();
 
-if ($exec) {
-    echo "The password has been changed successfully !";
-    header('Location: http://localhost/bdr/dashboard');
+$sql = "SELECT * FROM employe WHERE matriculeemploye = :matricule";
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':matricule', $_SESSION['matricule']);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+$derniereco = $user['DDERNIERECOEMPLOYE'];
+if ($derniereco != null) {
+    $sql = "UPDATE `employe` SET `DDERNIERECOEMPLOYE` = :date WHERE `matriculeemploye` = :matricule";
+    $exec = $pdo->prepare($sql);
+    $exec->bindValue(':matricule', $_SESSION['matricule']);
+    $exec->bindValue(':date', date("Y-m-d"));
+    $exec->execute();
+    header('Location: /dashboard');
+    echo "You have successfully changed your password !";
 } else {
-    echo "The password has not been changed !";
-    header('Location: http://localhost/bdr/login/premiereConnexion.html');
+    echo "An error occured !";
 }
 
 ?>
