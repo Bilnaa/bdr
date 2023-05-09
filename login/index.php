a0a5615002e8f4ef049401172ddeca520051f791
Bilnaa <noufele.mehdi@hotmail.fr>
2023-05-09 04:34:30 +0200

    fix : wrong path

--- login/index.php
+++ login/index.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bois Du Roy - Login - Intranet</title>
-    <link rel="shortcut icon" href="../public/img/logo_boi_du_roy.png" type="image/x-icon">
-    <link rel="apple-touch-icon" sizes="180x180" href="../public/img/logo_boi_du_roy.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../public/img/logo_boi_du_roy.png">
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="shortcut icon" href="/public/img/logo_boi_du_roy.png" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/img/logo_boi_du_roy.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public/img/logo_boi_du_roy.png">
+    <link rel="stylesheet" href="/public/css/main.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
